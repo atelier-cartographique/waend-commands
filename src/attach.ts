d6ce38fc3a876717771a3edee039b496df34d800
Pierre Marchand <pierremarc07@gmail.com>
2017-03-16 09:56:54 +0100

    updated attach

--- src/attach.ts
+++ src/attach.ts
@@ -1,21 +1,49 @@
-import Promise from 'bluebird';
+import * as Promise from 'bluebird';
+import { ICommand, ISys, Context } from "waend-shell";
 
-function attach (path) {
-    let comps = path.split('/');
-    if (comps[0].length === 0) {
-        comps = comps.slice(1);
-    }
-    const uid = comps[0];
-    const gid = comps[1];
-    const lid = comps[2];
-    if(!!uid && !!gid && !!lid){
-        return this.binder.attachLayerToGroup(uid, gid, lid);
-    }
-    return Promise.reject('wrong argument, expecting /user_id/group_id/layer_id');
+
+interface Components {
+    user: string;
+    group?: string;
+    layer?: string;
+    feature?: string;
 }
 
 
-export default {
+const getPathComponents: (a: string) => (Components | null) =
+    (path) => {
+        const comps = path.split('/');
+        if (comps.length < 1) {
+            return null;
+        }
+
+        return {
+            user: comps[0],
+            group: comps[1],
+            layer: comps[2],
+            feature: comps[3],
+        }
+    }
+
+const attach: (a: Context, b: ISys, c: string[]) => Promise<any> =
+    (ctx, _sys, argv) => {
+        if (argv.length < 2) {
+            return Promise.reject(new Error('TooFewArguments'));
+        }
+
+        const groupComps = getPathComponents(ctx.resolve(argv[0]));
+        const layerComps = getPathComponents(ctx.resolve(argv[1]));
+
+
+        if (groupComps && layerComps && groupComps.group && layerComps.layer) {
+            return Context.binder.attachLayerToGroup(
+                groupComps.user, groupComps.group, layerComps.layer);
+        }
+        return Promise.reject('wrong argument, expecting groupPath layerPath');
+    }
+
+
+export const command: ICommand = {
     name: 'attach',
     command: attach
 };
