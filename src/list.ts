0f81e54d97063b867342a106d6004b880a429974
Pierre Marchand <pierremarc07@gmail.com>
2017-03-21 16:06:14 +0100

    forgot file

---
+++ src/list.ts
@@ -0,0 +1,89 @@
+
+
+import * as Promise from 'bluebird';
+import { Context, ISys, ICommand } from "waend-shell";
+import { Model } from "waend-lib";
+import { dom, getPathComponents, Components, LayerComponents, GroupComponents } from 'waend-util';
+
+const { getDomForModel } = dom;
+
+const printModel: (a: ISys) => (b: Model) => void =
+    (sys) => (model) => {
+        sys.stdout.write([{
+            text: model.id,
+            fragment: getDomForModel(model, 'name', 'div', 'model-name')
+        }])
+    };
+
+
+const listGroups: (a: Context, b: ISys, c: Components) => Promise<any> =
+    (ctx, sys, components) => {
+        return (
+            ctx.binder
+                .getGroups(components.user)
+                .then((groups) => {
+                    groups.forEach(printModel(sys));
+                    return groups;
+                }));
+    }
+
+
+const listLayers: (a: Context, b: ISys, c: GroupComponents) => Promise<any> =
+    (ctx, sys, components) => {
+        return (
+            ctx.binder
+                .getLayers(components.user, components.group)
+                .then((layers) => {
+                    layers.forEach(printModel(sys));
+                    return layers;
+                }));
+    }
+
+
+const listFeatures: (a: Context, b: ISys, c: LayerComponents) => Promise<any> =
+    (ctx, sys, components) => {
+        return (
+            ctx.binder
+                .getFeatures(components.user, components.group, components.layer)
+                .then((features) => {
+                    features.forEach(printModel(sys));
+                    return features;
+                }));
+    }
+
+
+
+const list: (a: Context, b: ISys, c: string[]) => Promise<any> =
+    (ctx, sys, argv) => {
+
+        if (argv.length === 0) {
+            return Promise.reject(new Error('MissingArguments'));
+        }
+
+        const components = getPathComponents(ctx.resolve(argv[0]));
+
+        if (!components) {
+            return Promise.reject(new Error('InvalidPath'));
+        }
+
+        if (components.pathType === 'feature') {
+            return Promise.resolve([]); // do we throw an error?
+        }
+        else if (components.pathType === 'layer') {
+            return listFeatures(ctx, sys, components);
+        }
+        else if (components.pathType === 'group') {
+            return listLayers(ctx, sys, components);
+        }
+        else {
+            return listGroups(ctx, sys, components);
+        }
+
+    }
+
+
+export const command: ICommand = {
+    name: 'ls',
+    command: list
+};
+
