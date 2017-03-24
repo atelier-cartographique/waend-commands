5eb62f35f06dd84b561106a1771f271185385f74
Pacome Beru <pacome.beru@gmail.com>
2017-03-24 16:26:33 +0100

    added licence notices in files

--- src/attach.ts
+++ src/attach.ts
@@ -1,3 +1,28 @@
+/*
+ * src/attach.ts
+ *
+ * 
+ * Copyright (C) 2015-2017 Pierre Marchand <pierremarc07@gmail.com>
+ * Copyright (C) 2017 Pacôme Béru <pacome.beru@gmail.com>
+ *
+ *  License in LICENSE file at the root of the repository.
+ *
+ *  This file is part of waend-command package.
+ *
+ *  waend-command is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3 of the License.
+ *
+ *  waend-command is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with waend-command.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 import * as Promise from 'bluebird';
 import { ICommand, ISys, Context } from "waend-shell";
 import { getPathComponents } from 'waend-util';
